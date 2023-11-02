7ab6f31782f7df2267d55b5c5dd660045d1cd6c6
Bruno Wünsch <bruno_wunsch@hotmail.com>
2023-11-02 16:26:21 -0300

    Added loading pages

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/loading.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Loader from "@/components/ui/Loader";
+
+const Loading = () => {
+  return ( 
+    <div className="flex h-full w-full items-center justify-center">
+      <Loader />
+    </div>
+   );
+};
+export default Loading;
